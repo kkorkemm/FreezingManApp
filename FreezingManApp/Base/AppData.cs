fb9d81d71a5ef26326a45f654416bedf362d3055
korkem <73012254+kkorkemm@users.noreply.github.com>
2021-07-13 19:30:15 +0600

    basis

---
+++ FreezingManApp/Base/AppData.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FreezingManApp.Base
+{
+    class AppData
+    {
+        /// <summary>
+        /// Контекст данных
+        /// </summary>
+        private static FreezingManDBEntities context;
+
+        /// <summary>
+        /// Получение контекста данных
+        /// </summary>
+        /// <returns> Контекст данных </returns>
+        public static FreezingManDBEntities GetContext()
+        {
+            if (context == null)
+            {
+                context = new FreezingManDBEntities();
+            }
+
+            return context;
+        }
+    }
+}
---
+++ FreezingManApp/ViewModel/MainViewModel.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FreezingManApp.ViewModel
+{
+    using MVVMCore;
+
+    class MainViewModel : INotify
+    {
+        private object currentView;
+
+        public object CurrentView
+        {
+            get { return currentView; }
+            set 
+            { 
+                currentView = value;
+                OnPropertyChanged();
+            }
+        }
+
+
+        public ListViewModel ListVM { get; set; }
+        public MapViewModel MapVM { get; set; }
+
+
+        public BaseCommand ListCommand { get; set; }
+        public BaseCommand MapCommand { get; set; }
+
+
+        public MainViewModel()
+        {
+            ListVM = new ListViewModel();
+            MapVM = new MapViewModel();
+
+            CurrentView = ListVM;
+
+            ListCommand = new BaseCommand(o =>
+            {
+                CurrentView = ListVM;
+            });
+
+            MapCommand = new BaseCommand(o =>
+            {
+               CurrentView = MapVM;
+            });
+        }
+    }
+}
